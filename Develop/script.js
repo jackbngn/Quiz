8a1d46b37d1c12882806e941e8a43a927f39b2f6
jack nguyen <jackngn83@gmail.com>
2023-01-23 04:29:10 -0800

    displayed questions and answers

--- Develop/script.js
+++ Develop/script.js
@@ -4,26 +4,31 @@ var questionCard = document.querySelector("#card-question")
 var nextButton = document.querySelector("#next-button")
 var startCard = document.querySelector(".card-start")
 var timer = document.querySelector("#timer")
+var scoreCard = document.querySelector("#high-score-card")
+
+
 
 
 
 // hide cards 
 function hideCards() {
-startCard.setAttribute("hidden", true );
+startCard.setAttribute("hidden", true);
 }
 
 // start button on click
 startButton.addEventListener("click", startQuiz);
 console.log("start");
+nextButton.addEventListener("click", nextQuestions)
 
+// nextButton.addEventListener("click", nextQuestions);
+// currentQuestionIndex++;
 
 //start quiz function
 function startQuiz() {
     hideCards();
     questionCard.classList.remove("hide");
-    nextButton.classList.remove("hide");
     countdown();
-    setQuestions()
+    showQuestions();
 }
 // add timer
 var timeLeft = 60;
@@ -42,33 +47,150 @@ function countdown() {
 }, 1000);
 }
 
-//question functions
-var questions = [ {
-      questions: "1. Inside which HTML element do we put the JavaScript?",
-      answers: [ "a) <javascript>",  "b) <js>", "c) <script>" , "d) <scripting></scripting>" ]
+function endQuiz (){
+    scoreCard.classList.remove("hide")
+    questionCard.setAttribute("hidden", true);
 }
-]
+
+function nextQuestions() {
+    if (currentQuestionIndex < questions.length) {
+        showQuestions();
+    } else {
+        endQuiz();
+    }
+
+
+}
+
+
+
+// question arrays
+
+//if answered corrected, button will add the class correct to make the button green.
+
+// if answered incorrected, button will add the class incorrect to make the button red.
 
 
 
 
+// function game() {
+    var currentQuestionIndex = 0;
+
+    //question functions
+    var questions = [ {
+          question: "Inside which HTML element do we put the JavaScript?",
+          answers: ["JavaScript", "<js>", "<script>", "<scripting></scripting>"],
+          correct: 2
+    }, {
+        question: 'How do you write "Hello World" in an alert box?',
+        answers: ['alert("Hello World")', 'msgBox("Hello World")', ' alertBox="Hello World"', 'alertBox("Hello World")'],
+        correct: 0
+    }, {
+        question: "How do you create a function?",
+        answers: ["function:myFunction()", "function=myFunction()", "function myFunction()", "myFunction():function"],
+        correct: 2
+    }
+    ]
+    
+    function correctAnswerEvent(button) {
+        button.classList.add("correct");
+        nextButton.classList.remove('hide')
+    }
+    
+    function incorrectAnswerEvent(button) {
+        button.classList.add('incorrect');
+        timeLeft -=2;
+    }
+    
+    function showQuestions() {
+        shuffledQuestions();
+
+        while (currentQuestionIndex < questions.length && timeLeft > 0) {
+            var { question, answers, correct } = questions[currentQuestionIndex];
+    
+          // update question
+          var questionSlot = document.querySelector("#questions");
+          questionSlot.innerText = question;
+          
+          // update answer selections
+          // get all answer buttons
+          var answer0 = document.querySelector("#answer0")
+          answer0.innerText = answers[0]
+          answer0.addEventListener("click", () => {
+            if (correct === 0) {
+                correctAnswerEvent(answer0)
+            } else {
+                incorrectAnswerEvent(answer0)
+            }
+          })
+          
+          var answer1 = document.querySelector("#answer1")
+          answer1.innerText = answers[1]
+          answer1.addEventListener("click", () => {
+            if (correct === 1) {
+                correctAnswerEvent(answer1)
+            } else {
+                incorrectAnswerEvent(answer1)
+            }
+          })
+    
+          var answer2 = document.querySelector("#answer2")
+          answer2.innerText = answers[2]
+          answer2.addEventListener("click", () => {
+            if (correct === 2) {
+                correctAnswerEvent(answer2)
+            } else {
+                incorrectAnswerEvent(answer2)
+            }
+          })
+          var answer3 = document.querySelector("#answer3")
+          answer3.innerText = answers[3]
+          answer3.addEventListener("click", () => {
+            if (correct === 3) {
+                correctAnswerEvent(answer3)
+            } else {
+                incorrectAnswerEvent(answer3)
+            }
+          })
+    
+          currentQuestionIndex++;
+        }
+    
+        // after the game
+        // get initials
+        // render high scores
+    }
+
+
+// function to shuffle the question randomly
+function shuffledQuestions() {
+    for (var i = questions.length - 1; i > 0; i--) {
+        var currentQuestionIndex = questions[i];
+        var randomIndex = Math.floor(Math.random() * 4 - 1);
+        var randomQuestion = questions[randomIndex];
+        questions[i] = randomQuestion; 
+        questions[randomIndex] = currentQuestionIndex;
+    }
+}
+
+
+ // after the game
+    // get initials
+    // render high scores
 
 
 
+// function shuffledAnswers() {
+//     for (var i = 0; i < 4; i++) {
+//         var CurrentAnswerIndex = questions.answers[i];
+//         var randomAnswerIndex = Math.floor(Math.random() * 4 - 1);
+//         var randomAnswer = questions[randomAnswerIndex];
+//         answers[i] = randomAnswer;
+//         answers[randomAnswerIndex] = CurrentAnswerIndex
+//     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// }
+// console.log(questions['answers'])
 
 // questions 
 // 1.Inside which HTML element do we put the JavaScript?
@@ -108,8 +230,4 @@ var questions = [ {
 // a) Method
 // b) assignment operator
 // c) Variable
-// d) string
-
-
-// high score 
-
+// d) string 
